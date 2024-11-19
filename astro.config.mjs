0836b7ad42152c5ff2e21940b09dd90fc4c0ce16
Ritu Raj <theriturajps@gmail.com>
2024-11-19 19:32:20 +0530

    #updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://riturajps.vercel.app",
+  site: "https://www.riturajps.in/",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
