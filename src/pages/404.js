104c946a1efde010876b92a8eb0f86bebfdf4220
Ritu Raj Pratap Singh <theriturajps@gmail.com>
2024-11-22 00:54:36 +0530

    Delete src/pages/404.js

--- src/pages/404.js
+++
@@ -1,9 +0,0 @@
-export default function Custom404() {
-    return (
-        <div style={{ textAlign: "center", marginTop: "50px" }}>
-            <h1>404 - Page Not Found</h1>
-            <p>The page you’re looking for doesn’t exist.</p>
-            <a href="/">Go back home</a>
-        </div>
-    );
-}
